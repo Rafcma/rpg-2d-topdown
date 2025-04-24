98ecc2c7b0464fb99ab4eb8bdd8403f89a3ec449
Rafcma <rafa_montealegre@hotmail.com>
2025-04-24 06:23:16 -0300

    Segundo-commit-topdown-2d
    -NPC (Scripts)
    -Modificações UI do editor

---
+++ Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [Header("Components")]
+    public GameObject dialogueObj; // janela do dialogo
+    public Image profileSprite; // sprite do perfil
+    public Text speechText; // texto da fala
+    public Text actorNameText; // nome do npc
+
+    [Header("Settings")]
+    public float typingSpeed; // velocidade da fala
+
+    //váriaveis de controle
+    private bool isShowing; // mostra se a janela está ativa
+    private int index; // contagem para falas (index de loop)
+    private string[] sentences;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+
+    // pular para próxima fala
+    public void NextSentence()
+    {
+
+    }
+
+    // chamar a fala
+    public void Speech(string[] txt)
+    {
+        if (!isShowing)
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+}
